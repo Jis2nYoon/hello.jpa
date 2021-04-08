593de4b6394882a3d959fd631520a5ab56e21642
yoosi <yoosijisun12@gmail.com>
2021-04-08 10:40:28 +0900

    6강 JPA 내부구조 (5) - 준영속 상태 (지연로딩을 사용 할 수 없다)

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -34,13 +34,13 @@ public class Main {
 			em.persist(memberB);
 			em.persist(memberC);
 			
-			//JPQL 쿼리 실행시 플러시가 자동으로 호출되는 이유
-			// 중간에 JPQL 실행  이건 JPA가 제공하는 것이라 문제가 없음. BUT Mybatis, springJDBC 템플릿 같은걸 섞어 쓴다면 직접 flush를 해줘야한다.
-			// 플러시는 영속성 컨텍스트를 비우는게 아니다, db와 동기화 하는 것임.
-			TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-			List<Member> members = query.getResultList();
-
-			em.remove(memberA); //엔티티 삭제
+			em.flush();
+			
+			em.detach(memberA); //특정 엔티티만 준영속 상태로 전환
+			//em.clear(); // 준영속 상태
+			//em.close(); // 영속성 컨텍스트를 종료
+			
+			memberA.setName("memberAA");
 			
 			tx.commit(); // [트랜잭션] 커밋
 		} catch (Exception e){
